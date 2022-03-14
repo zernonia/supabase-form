25e55ffb3ae24ec84dca47a47a0f33ad314b2907
zernonia <zernonia@gmail.com>
2022-03-14 12:42:21 +0800

    add toggle component

--- components.d.ts
+++ components.d.ts
@@ -5,6 +5,7 @@
 declare module 'vue' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    Toggle: typeof import('./src/components/Toggle.vue')['default']
   }
 }
 
---
+++ src/components/Toggle.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = defineProps({
+  modelValue: Boolean,
+})
+const emits = defineEmits(["update:modelValue"])
+
+const onChange = (ev: Event) => {
+  if (!ev.target) return
+  let target = ev.target as HTMLInputElement
+  emits("update:modelValue", target.checked)
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-center w-full mb-12">
+    {{ modelValue }}
+    <label class="relative cursor-pointer">
+      <input class="absolute opacity-0" type="checkbox" :value="modelValue" @change="onChange" />
+
+      <div class="block bg-warm-gray-100 w-10 h-6 rounded-full"></div>
+
+      <div class="dot absolute left-1 top-1 bg-white w-4 h-4 shadow rounded-full transition"></div>
+    </label>
+  </div>
+</template>
+
+<style lang="postcss">
+input:checked ~ .dot {
+  @apply transform translate-x-full bg-green-400;
+}
+</style>
