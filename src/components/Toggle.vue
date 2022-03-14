0c562f26f35a5cb6f6c0052f095f0e3773b7a4ba
zernonia <zernonia@gmail.com>
2022-03-14 13:26:36 +0800

    update component style

--- src/components/Toggle.vue
+++ src/components/Toggle.vue
@@ -12,20 +12,22 @@ const onChange = (ev: Event) => {
 </script>
 
 <template>
-  <div class="flex items-center justify-center w-full mb-12">
-    {{ modelValue }}
+  <div class="flex items-center justify-center w-full">
     <label class="relative cursor-pointer">
       <input class="absolute opacity-0" type="checkbox" :value="modelValue" @change="onChange" />
 
-      <div class="block bg-warm-gray-100 w-10 h-6 rounded-full"></div>
+      <div class="toggle-bg block bg-warm-gray-100 w-10 h-6 rounded-full transition"></div>
 
-      <div class="dot absolute left-1 top-1 bg-white w-4 h-4 shadow rounded-full transition"></div>
+      <div class="toggle-dot absolute left-1 top-1 bg-white w-4 h-4 shadow rounded-full transition"></div>
     </label>
   </div>
 </template>
 
 <style lang="postcss">
-input:checked ~ .dot {
-  @apply transform translate-x-full bg-green-400;
+input:checked ~ .toggle-bg {
+  @apply bg-green-400;
+}
+input:checked ~ .toggle-dot {
+  @apply transform translate-x-full;
 }
 </style>
