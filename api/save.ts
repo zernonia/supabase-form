42e5aaf14a806ac213fa6abbe1c30ad52d57845d
zernonia <zernonia@gmail.com>
2022-03-22 00:00:58 +0800

    update return single response

--- api/save.ts
+++ api/save.ts
@@ -5,7 +5,10 @@ export default async (request: VercelRequest, response: VercelResponse) => {
   const body = JSON.parse(request.body)
   const slug = Math.random().toString(36).substring(2, 10)
 
-  const { data, error } = await supabase.from("forms").insert({ ...body, slug })
+  const { data, error } = await supabase
+    .from("forms")
+    .insert({ ...body, slug })
+    .single()
   if (error) {
     response.status(400).send(error)
   } else {
