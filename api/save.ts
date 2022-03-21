4980d6909a72866ce34e3768cc1e69bf9cae4013
zernonia <zernonia@gmail.com>
2022-03-21 23:54:51 +0800

    add save api

---
+++ api/save.ts
@@ -0,0 +1,14 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node"
+import { supabase } from "./lib/supabase"
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  const body = JSON.parse(request.body)
+  const slug = Math.random().toString(36).substring(2, 10)
+
+  const { data, error } = await supabase.from("forms").insert({ ...body, slug })
+  if (error) {
+    response.status(400).send(error)
+  } else {
+    response.json(data)
+  }
+}
