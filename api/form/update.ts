a20a06a9bdf3959a1388810b80b4127153ba68e9
zernonia <zernonia@gmail.com>
2022-03-22 23:06:48 +0800

    update api route

--- api/form/update.ts
+++ api/form/update.ts
@@ -1,14 +1,10 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node"
-import { supabase } from "./lib/supabase"
+import { supabase } from "../_lib/supabase"
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   const body = JSON.parse(request.body)
-  const slug = Math.random().toString(36).substring(2, 10)
 
-  const { data, error } = await supabase
-    .from("forms")
-    .insert({ ...body, slug })
-    .single()
+  const { data, error } = await supabase.from("forms").update(body).eq("slug", body.slug).single()
   if (error) {
     response.status(400).send(error)
   } else {
