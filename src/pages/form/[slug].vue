dff6d648e756d0a9dea45e2ec66bd3de85c7da77
zernonia <zernonia@gmail.com>
2022-03-24 23:00:15 +0800

    form loading page

--- src/pages/form/[slug].vue
+++ src/pages/form/[slug].vue
@@ -6,7 +6,8 @@ import { useRoute } from "vue-router"
 import { useFetch } from "@vueuse/core"
 
 const route = useRoute()
-
+const isSubmitting = ref(false)
+const isSuccessful = ref(false)
 const { isFetching, data } = useFetch<{ config: Config }>("/api/form/get", {
   method: "POST",
   body: JSON.stringify({
@@ -15,6 +16,7 @@ const { isFetching, data } = useFetch<{ config: Config }>("/api/form/get", {
 }).json()
 
 const submitForm = async (form: FormInput) => {
+  isSubmitting.value = true
   fetch("/api/form/submit", {
     method: "POST",
     body: JSON.stringify({
@@ -22,14 +24,27 @@ const submitForm = async (form: FormInput) => {
       form,
     }),
   })
+    .then((res) => res.json())
+    .then((res) => {
+      isSuccessful.value = true
+    })
+    .finally(() => {
+      isSubmitting.value = false
+    })
 }
 </script>
 
 <template>
-  <div>
-    <div v-if="isFetching">Loading</div>
-    <Form v-else-if="data?.config" :config="data.config" @submit="submitForm"></Form>
-    <div v-else>No form is found</div>
+  <div class="min-h-screen min-w-screen flex bg-gray-50">
+    <div class="self-center justify-self-center text-center w-full text-4xl" v-if="isFetching">Loading...</div>
+    <Form
+      v-else-if="data?.config"
+      :config="data.config"
+      @submit="submitForm"
+      :isSuccessful="isSuccessful"
+      :isSubmitting="isSubmitting"
+    ></Form>
+    <div v-else class="self-center justify-self-center text-center w-full text-4xl">No form is found 😢</div>
   </div>
 </template>
 
