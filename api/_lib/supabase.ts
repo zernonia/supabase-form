d1eda3c3d1fd4791d6e9e725545487679205b03c
zernonia <zernonia@gmail.com>
2022-03-24 22:37:13 +0800

    use service key on vercel function

--- api/_lib/supabase.ts
+++ api/_lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js"
 
 export const supabase = createClient(
   process.env.VITE_SUPABASE_URL as string,
-  process.env.VITE_SUPABASE_ANON_KEY as string
+  process.env.VITE_SUPABASE_SERVICE_KEY as string
 )
