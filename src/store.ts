63b200fa680cc6c869e23d5f4eafb4cf8b7d5921
zernonia <zernonia@gmail.com>
2022-03-24 21:48:21 +0800

    add forms to store

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,8 @@
 import { User } from "@supabase/gotrue-js"
 import { reactive } from "vue"
+import { Forms } from "./interface"
 
 export const store = reactive({
   user: {} as User | null | undefined,
+  forms: [] as Forms[],
 })
