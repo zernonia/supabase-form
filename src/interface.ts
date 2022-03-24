3b54a54fa82848294a55f780e5782acb53c6811e
zernonia <zernonia@gmail.com>
2022-03-24 12:00:23 +0800

    add interface from database

--- src/interface.ts
+++ src/interface.ts
@@ -34,3 +34,31 @@ export interface Config {
   logo?: string
   column: ConfigColumn[]
 }
+
+// Generated from supabase-schema.vercel.app
+export interface Users {
+  id: string /* primary key */
+  email?: string
+}
+
+export interface Projects {
+  id: string /* primary key */
+  user_id: string /* foreign key to users.id */
+  url?: string
+  anon_key?: string
+  jwt_token?: string
+  created_at?: string
+  definitions?: any // type unknown;
+  users?: Users
+}
+
+export interface Forms {
+  id: number /* primary key */
+  slug?: string
+  config?: any // type unknown;
+  created_at?: string
+  user_id?: string /* foreign key to users.id */
+  project_id?: string /* foreign key to projects.id */
+  users?: Users
+  projects?: Projects
+}
