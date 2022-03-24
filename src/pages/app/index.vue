6f01db91aac5ad9f837c87b52c6ba9ea7474317b
zernonia <zernonia@gmail.com>
2022-03-24 21:48:36 +0800

    update app index page with forms

--- src/pages/app/index.vue
+++ src/pages/app/index.vue
@@ -1,3 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Forms } from "@/interface"
+import { supabase } from "@/plugins/supabase"
+import { onMounted, ref } from "vue"
+import { store } from "@/store"
 
-<template>index</template>
+const projects = ref()
+
+const fetchForms = async () => {
+  const { data, error } = await supabase.from<Forms>("forms").select("*").order("created_at", { ascending: false })
+  if (data) {
+    store.forms = data
+  }
+}
+
+fetchForms()
+</script>
+
+<template>
+  <div>
+    <section>
+      <div class="flex space-x-2 items-center">
+        <h2>Forms</h2>
+        <router-link class="button" to="/app/create">Add form</router-link>
+      </div>
+
+      <ul class="flex items-center mt-4">
+        <template v-for="form in store.forms">
+          <router-link :to="`/app/${form.slug}`" class="p-12 bg-gray-100 mr-2">
+            {{ form.slug }}
+          </router-link>
+        </template>
+      </ul>
+    </section>
+  </div>
+</template>
