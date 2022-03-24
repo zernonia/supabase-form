9a91f621071ca09480a7698aec18b03bcc86bd81
zernonia <zernonia@gmail.com>
2022-03-24 20:51:23 +0800

    separate create/slug page

--- api/form/get.ts
+++ api/form/get.ts
@@ -4,7 +4,7 @@ import { supabase } from "../_lib/supabase"
 export default async (request: VercelRequest, response: VercelResponse) => {
   const body = JSON.parse(request.body)
 
-  const { data, error } = await supabase.from("forms").select("config, key").eq("slug", body.slug).single()
+  const { data, error } = await supabase.from("forms").select("config").eq("slug", body.slug).single()
   if (error) {
     response.status(400).send(error)
   } else {
--- src/pages/app/create.vue
+++ src/pages/app/create.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, watch } from "vue"
 import { Column, Config, ConfigColumn, TableState, Table, Projects, Forms } from "@/interface"
-import { formatTitle, applyDrag } from "@/utils"
+import { formatDefinitions, formatTitle, applyDrag, addColumnToConfig } from "@/utils"
 //@ts-ignore
 import { Container, Draggable } from "vue-dndrop"
 import { useRoute, useRouter } from "vue-router"
@@ -30,6 +30,41 @@ const projects = ref<Projects[]>([])
 const selectedProject = ref<Projects>()
 
 const isPreviewing = ref(false)
+const isEditing = ref(false)
+
+const fetchSchema = () => {
+  fetch(`${supabaseInfo.value.url}/rest/v1/?apikey=${supabaseInfo.value.anon_key}`)
+    .then((res) => res.json())
+    .then((res) => {
+      const { definitions, paths } = res
+      insertProject({ definitions, paths })
+      tables.value = formatDefinitions({ definitions, paths })
+    })
+}
+
+const insertProject = async (definitions: Projects["definitions"]) => {
+  const { data, error } = await supabase.from<Projects>("projects").upsert({
+    user_id: store.user?.id,
+    url: supabaseInfo.value.url,
+    anon_key: supabaseInfo.value.anon_key,
+    definitions,
+  })
+  console.log({ data })
+}
+
+const fetchProjects = async () => {
+  const { data, error } = await supabase.from<Projects>("projects").select("*")
+  if (data) {
+    projects.value = data
+  }
+}
+
+const selectTable = () => {
+  if (!selectedTable.value?.columns) return
+  config.value.title = formatTitle(selectedTable.value.title)
+  config.value.column = selectedTable.value.columns.filter((i) => i.required).map((i) => addColumnToConfig(i))
+  availableColumn.value = selectedTable.value.columns.filter((i) => !i.required)
+}
 
 const onDrop = (ref: "configColumn" | "availableColumn", dropResult: any) => {
   if (!config.value.column) return
@@ -42,28 +77,61 @@ const onDrop = (ref: "configColumn" | "availableColumn", dropResult: any) => {
 
 const save = async () => {
   const slug = Math.random().toString(36).substring(2, 10)
+
   const { data, error } = await supabase
     .from<Forms>("forms")
     .insert({ config: config.value, slug, user_id: store.user?.id, project_id: selectedProject.value?.id })
     .single()
 
   if (data) {
-    router.replace(`/app/${data.slug}`)
+    router.replace({ path: `/app/${data.slug}` })
   }
 }
 
-const fetchData = async () => {
-  const { data, error } = await supabase.from<Forms>("forms").select("*").eq("slug", route.params.slug).single()
+watch(selectedProject, (n) => {
+  tables.value = formatDefinitions(n?.definitions)
+  config.value = {
+    title: "",
+    logo: "https://www.madewithsupabase.com/_nuxt/logo.fbcda856.svg",
+    column: [],
+  }
+})
 
-  if (data) config.value = data.config
-}
-
-fetchData()
+fetchProjects()
 </script>
 
 <template>
   <div class="flex flex-col">
-    <div class="flex">
+    <div class="flex items-center">
+      <Modal v-if="isEditing" @close="isEditing = false">
+        <div class="max-w-72 p-4 bg-gray-50 border rounded-xl">
+          <label class="label" for="supabase-url">Url</label>
+          <input class="input" name="supabase-url" type="url" v-model="supabaseInfo.url" />
+
+          <label class="label" for="supabase-key">Anon</label>
+          <input class="input" name="supabase-key" type="text" v-model="supabaseInfo.anon_key" />
+
+          <label class="label" for="supabase-jwt">JWT Token</label>
+          <input class="input" name="supabase-jwt" type="text" v-model="supabaseInfo.jwt_token" />
+
+          <button class="button" @click="fetchSchema">Fetch</button>
+        </div>
+      </Modal>
+
+      <select class="input max-w-72" v-model="selectedProject" @change="selectTable">
+        <option disabled value="undefined">Please select one</option>
+        <option v-for="project in projects" :value="project">
+          {{ project.id }}
+        </option>
+      </select>
+
+      <select class="input max-w-72" v-model="selectedTable" @change="selectTable">
+        <option disabled value="undefined">Please select one</option>
+        <option v-for="table in tables" :value="table">
+          {{ table.title }}
+        </option>
+      </select>
+
       <button class="button" @click="isPreviewing = true">Preview</button>
       <button class="button bg-green-600" @click="save">Save</button>
     </div>
