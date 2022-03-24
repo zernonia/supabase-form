eb6ee3bccaba23c9f72f271f50ef98ae5fc4718c
zernonia <zernonia@gmail.com>
2022-03-24 10:38:19 +0800

    add supabase onAuthChange

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import { onBeforeMount, onMounted } from "vue"
+import { supabase } from "./plugins/supabase"
+import { store } from "@/store"
+
+onBeforeMount(() => {
+  supabase.auth.onAuthStateChange((event, session) => {
+    console.log({ event, session })
+    if (event == "SIGNED_IN") {
+      store.user = session?.user
+    } else if (event == "SIGNED_OUT") {
+      store.user = null
+    }
+  })
+})
+</script>
+
 <template>
   <router-view></router-view>
 </template>
