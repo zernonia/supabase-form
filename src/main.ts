bea5cec4a86fd72f5c966049102ce9e4f0997cff
zernonia <zernonia@gmail.com>
2022-03-24 10:37:36 +0800

    add router guard

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,22 @@ import generatedRoutes from "virtual:generated-pages"
 import "./assets/main.css"
 import "virtual:windi.css"
 import { plugin, defaultConfig } from "@formkit/vue"
+import { store } from "./store"
 
 const routes = setupLayouts(generatedRoutes)
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, from) => {
+  const toName = to.name?.toString() ?? ""
+  if (!store.user && toName.includes("app")) {
+    return { name: "index" }
+  } else if (store.user && ["index", "login"].includes(toName || "")) {
+    return { name: "app" }
+  }
 })
 
 createApp(App).use(router).use(plugin, defaultConfig).mount("#app")
