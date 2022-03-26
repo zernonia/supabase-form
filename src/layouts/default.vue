55c691401a66ed70bcb0d09dc9b4c07f57b19522
zernonia <zernonia@gmail.com>
2022-03-26 12:45:01 +0800

    improve app index

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex flex-col min-h-screen p-8">
-    <router-link to="/" class="font-semibold text-4xl">Supabase Form</router-link>
+    <router-link to="/" class="font-semibold text-4xl w-max p-2 rounded-xl bg-transparent hover:bg-light-400 transition"
+      >Supaform</router-link
+    >
     <router-view></router-view>
   </div>
 </template>
--- src/pages/app/index.vue
+++ src/pages/app/index.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
 import { Forms } from "@/interface"
 import { supabase } from "@/plugins/supabase"
-import { onMounted, ref } from "vue"
+import { ref } from "vue"
 import { store } from "@/store"
 
 const projects = ref()
-
+const isLoading = ref(false)
 const fetchForms = async () => {
-  const { data, error } = await supabase.from<Forms>("forms").select("*").order("created_at", { ascending: false })
+  isLoading.value = true
+  const { data, error } = await supabase
+    .from<Forms>("forms")
+    .select("*, projects(*)")
+    .order("created_at", { ascending: false })
   if (data) {
     store.forms = data
   }
+  isLoading.value = false
 }
 
 fetchForms()
@@ -18,19 +23,26 @@ fetchForms()
 
 <template>
   <div>
-    <section>
+    <section class="mt-4">
       <div class="flex space-x-2 items-center">
-        <h2>Forms</h2>
+        <h2 class="text-3xl font-bold">Forms</h2>
         <router-link class="button" to="/app/create">Add form</router-link>
       </div>
 
-      <ul class="flex items-center mt-4">
+      <ul v-if="store.forms.length" class="flex items-center mt-4">
         <template v-for="form in store.forms">
-          <router-link :to="`/app/${form.slug}`" class="p-12 bg-gray-100 mr-2">
+          <router-link :to="`/app/${form.slug}`" class="p-12 bg-gray-100 hover:bg-gray-200 transition mr-2 rounded-xl">
+            <h2 class="text-2xl font-bold">{{ form.config.title }}</h2>
+            <p class="">{{ form.projects?.name }} / {{ form.table_name }}</p>
             {{ form.slug }}
           </router-link>
         </template>
       </ul>
+
+      <div class="w-full h-48 flex items-center justify-center" v-else>
+        <i-eos-icons-bubble-loading v-if="isLoading" class="text-5xl"></i-eos-icons-bubble-loading>
+        <p v-else class="text-gray-400 text-2xl">No form 😮</p>
+      </div>
     </section>
   </div>
 </template>
