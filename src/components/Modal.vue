d362084b6ec6ab3375260ba595225da7c95cd5e4
zernonia <zernonia@gmail.com>
2022-03-26 10:57:12 +0800

    center item in modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -11,7 +11,7 @@ watch(escape, (v) => {
 </script>
 
 <template>
-  <div class="fixed top-0 left-0 w-screen h-screen bg-white bg-opacity-50 z-20">
+  <div class="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-white bg-opacity-50 z-20">
     <button class="p-4 absolute top-8 right-8 text-2xl text-green-500 z-10" @click="emits('close')">
       <i-mdi:close></i-mdi:close>
     </button>
