7c115579469539397074798cc7612a4dee8fdcf7
zernonia <zernonia@gmail.com>
2022-03-26 08:55:16 +0800

    fix router guard not working

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,8 @@
 import { User } from "@supabase/gotrue-js"
-import { reactive } from "vue"
+import { computed, reactive } from "vue"
 import { Forms } from "./interface"
 
 export const store = reactive({
-  user: {} as User | null | undefined,
+  user: null as User | null | undefined,
   forms: [] as Forms[],
 })
